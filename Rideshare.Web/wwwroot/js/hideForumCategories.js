2d8cc3696d0be70ea9fce1818b6300f7d7982619
ivanrk <ivanrk95@gmail.com>
2020-07-30 20:20:23 +0300

    Add hide category option using JS

---
+++ Rideshare.Web/wwwroot/js/hideForumCategories.js
@@ -0,0 +1,23 @@
+﻿function hide() {
+
+    let buttons = Array.from(document.querySelectorAll('i#hide'));
+
+    buttons.forEach(button => button.addEventListener('click', clickEvent));
+
+    function clickEvent(e) {
+        let button = e.target;
+        let table = button.parentNode.parentNode.parentNode.parentNode;
+        let tableHeadings = table.children[0].children[1];
+        let tableBody = table.children[1];
+
+        if (button.className === 'fa fa-minus-circle') {
+            button.className = 'fa fa-plus-circle';
+            tableHeadings.hidden = true;
+            tableBody.hidden = true;
+        } else {
+            button.className = 'fa fa-minus-circle';
+            tableHeadings.hidden = false;
+            tableBody.hidden = false;
+        }
+    }
+}
